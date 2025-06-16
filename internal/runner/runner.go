b26185141d1f8b972a8859d3fb15b0ccd3038b39
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-06-16 23:53:31 +0300

    ✨ feat(runner): accepts `RunConfig`

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -1,22 +1,44 @@
 package runner
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/S1riyS/gotchex/internal/config"
 )
 
 type Runner struct {
-	command string
+	config *config.RunConfig
 }
 
-func New(command string) *Runner {
-	return &Runner{command: command}
+func New(cfg *config.RunConfig) *Runner {
+	return &Runner{config: cfg}
 }
 
 func (r *Runner) Run() error {
-	cmd := exec.Command("sh", "-c", r.command)
+	// Excute build command
+	if r.config.Build != nil {
+		fmt.Println("Building...")
+		err := setUpCmd(*r.config.Build).Run()
+		if err != nil {
+			return err
+		}
+	}
+
+	// Execute run command
+	fmt.Println("Running...")
+	err := setUpCmd(r.config.Run).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setUpCmd(command string) *exec.Cmd {
+	cmd := exec.Command("sh", "-c", command)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	return cmd.Run()
+	return cmd
 }
